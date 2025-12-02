f6f530cdafde226abebe130dc8e6fdea06fb0015
Patrick Bradley <pmbradley1997@gmail.com>
2025-12-02 13:46:48 -0500

    Adding new webpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "patbradley/server/auth";
 import { api, HydrateClient } from "patbradley/trpc/server";
+import Link from "next/link";
 
 export default async function Home() {
   const session = await auth();
@@ -11,12 +12,11 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center gap-12">
-        <h1 className="text-7xl font-bold text-white drop-shadow-2xl backdrop-blur-sm">
-          Welcome to my portfolio
-        </h1>
-        <button className="rounded-xl border border-white/20 px-16 py-8 text-4xl font-semibold text-white shadow-lg backdrop-blur-md transition-all hover:scale-105 hover:backdrop-blur-lg active:scale-95">
-          View My Work
-        </button>
+        <Link href="https://soundcloud.com/patrick-bradley-517513482">
+          <button className="rounded-xl border border-white/20 px-16 py-8 text-4xl font-semibold text-white shadow-lg backdrop-blur-md transition-all hover:scale-105 hover:backdrop-blur-lg active:scale-95">
+            View My Work
+          </button>
+        </Link>
       </main>
     </HydrateClient>
   );
